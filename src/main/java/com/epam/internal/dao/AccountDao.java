57ee59e2bd6978eee8ce46941729613f7ea76b2d
IbrayevRamil <ramil1dan@gmail.com>
2017-04-01 16:19:10 +0300

    Added AbstractDao, AccountDao and AccountService

--- src/main/java/com/epam/internal/dao/AccountDao.java
+++ src/main/java/com/epam/internal/dao/AccountDao.java
@@ -1,7 +1,9 @@
 package com.epam.internal.dao;
 
 import com.epam.internal.data.entities.Account;
+import org.springframework.stereotype.Repository;
 
+@Repository("accountDao")
 public class AccountDao extends AbstractDao<Account> implements IGenericDao<Account> {
 
     AccountDao(Class<Account> persistentClass) {
