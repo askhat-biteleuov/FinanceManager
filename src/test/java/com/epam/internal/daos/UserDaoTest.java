513cb803359d08432436fcfafab6e13a8a1cea59
Ivan_Fefilov <spb.fefilov.i@gmail.com>
2017-04-09 10:45:28 +0300

    Added test for UserDao

---
+++ src/test/java/com/epam/internal/daos/UserDaoTest.java
@@ -0,0 +1,32 @@
+package com.epam.internal.daos;
+
+import com.epam.internal.models.User;
+import com.epam.internal.models.UserInfo;
+import com.epam.internal.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+@ContextConfiguration("classpath:common-mvc-config.xml")
+public class UserDaoTest extends AbstractTestNGSpringContextTests {
+    @Autowired
+    private UserService userService;
+
+    private static User user;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        user = new User("user@email", "password", new UserInfo("name", "surname"));
+        userService.createUser(user);
+    }
+
+    @Test
+    public void testGetUserByEmail() throws Exception {
+        Assert.assertEquals(user.getEmail(), userService.findByEmail("user@email").getEmail());
+    }
+
+}
