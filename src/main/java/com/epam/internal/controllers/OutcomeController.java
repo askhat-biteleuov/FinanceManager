b862245d856334a0a9242bf57114bbde591b4866
yuhans <yushin.tema@gmail.com>
2017-04-11 20:31:31 +0300

    Adding Outcomes work correctly

--- src/main/java/com/epam/internal/controllers/OutcomeController.java
+++ src/main/java/com/epam/internal/controllers/OutcomeController.java
@@ -8,6 +8,7 @@ import com.epam.internal.services.AccountService;
 import com.epam.internal.services.OutcomeService;
 import com.epam.internal.services.OutcomeTypeService;
 import com.epam.internal.services.UserService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -22,6 +23,8 @@ import java.util.List;
 
 @Controller
 public class OutcomeController {
+    private static final Logger LOGGER = Logger.getLogger(OutcomeController.class);
+
     @Autowired
     private OutcomeTypeService outcomeTypeService;
     @Autowired
@@ -32,30 +35,44 @@ public class OutcomeController {
     private AccountService accountService;
 
     @RequestMapping(value = "/addoutcome", method = RequestMethod.GET)
-    public ModelAndView newIncome(WebRequest request) {
+    public ModelAndView newOutcome(WebRequest request) {
+        OutcomeDto outcomeDto = prepareDTO(request);
+        return new ModelAndView("newoutcome", "outcomeDto", outcomeDto);
+    }
+
+    @RequestMapping(value = "/addoutcome", method = RequestMethod.POST)
+    public ModelAndView addOutcome(@ModelAttribute("outcomeDto") OutcomeDto outcomeDto, BindingResult result, WebRequest request) {
+        User user = userService.getLoggedUser();
+        LOGGER.info("Нашли юзера");
+        if (/*!result.hasErrors() &&*/ user != null) {
+            LOGGER.info("Ошибок нет и юзер не равен нулл");
+            LOGGER.info("ID Аккаунта = " + outcomeDto.getAccountId());
+            LOGGER.info("ID outcomeType = " + outcomeDto.getOutcomeTypeId());
+            Account account = accountService.findAccountById(outcomeDto.getAccountId());
+            OutcomeType outcomeType = outcomeTypeService.findTypeById(outcomeDto.getOutcomeTypeId());
+            if (account != null && outcomeType != null) {
+                LOGGER.info("Аккаунт не равен нулл и тип расходов не равен нулл");
+                outcomeService.addOutcome(outcomeDto, account, outcomeType);
+                account.setBalance(account.getBalance().subtract(new BigDecimal(outcomeDto.getAmount())));
+                accountService.updateAccount(account);
+                return new ModelAndView("redirect:" + "/index");
+            }
+            LOGGER.info("Аккаунта или тип расходов = нулл");
+        }
+        LOGGER.info("юзер = нулл");
+        OutcomeDto freshDto = prepareDTO(request);
+        return new ModelAndView("newoutcome", "outcomeDto", freshDto);
+    }
+
+    public OutcomeDto prepareDTO(WebRequest request) {
         String accountId = request.getParameter("accountId");
         User user = userService.getLoggedUser();
         List<OutcomeType> availableOutcomeTypes = outcomeTypeService.getAvailableOutcomeTypes(user);
         OutcomeDto outcomeDto = new OutcomeDto();
         outcomeDto.setAccountId(Long.parseLong(accountId));
         outcomeDto.setOutcomeTypes(availableOutcomeTypes);
-        return new ModelAndView("newoutcome", "outcomeDto", outcomeDto);
-    }
-
-    @RequestMapping(value = "/addoutcome", method = RequestMethod.POST)
-    public ModelAndView addIncome(@ModelAttribute("outcomeDto") OutcomeDto outcomeDto, BindingResult result) {
-        User user = userService.getLoggedUser();
-        if (/*!result.hasErrors() && */user != null) {
-            Account account = accountService.findAccountById(outcomeDto.getAccountId());
-            OutcomeType outcomeType = outcomeTypeService.findTypeById(outcomeDto.getOutcomeTypeId());
-            if (account != null && outcomeType != null) {
-                outcomeService.addOutcome(outcomeDto, account, outcomeType);
-                account.setBalance(account.getBalance().subtract(new BigDecimal(outcomeDto.getAmount())));
-                accountService.updateAccount(account);
-                return new ModelAndView("redirect:" + "/index");
-            }
-        }
-        return new ModelAndView("newoutcome");
+        availableOutcomeTypes.forEach(type -> LOGGER.info(type.getId()));
+        return outcomeDto;
     }
 
 
