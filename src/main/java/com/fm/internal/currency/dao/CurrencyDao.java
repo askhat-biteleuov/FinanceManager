9c3f2a0b48bd3ba81aba29cd62866c124382814d
KeiZaal <gafarov.dilshod@gmail.com>
2017-05-03 22:16:37 +0300

    add getByCharCode method currencydao

--- src/main/java/com/fm/internal/currency/dao/CurrencyDao.java
+++ src/main/java/com/fm/internal/currency/dao/CurrencyDao.java
@@ -1,10 +1,32 @@
 package com.fm.internal.currency.dao;
 
 import com.fm.internal.currency.model.Currency;
+import com.fm.internal.currency.model.Currency_;
 import com.fm.internal.daos.GenericDao;
+import org.hibernate.Session;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.NoResultException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 public class CurrencyDao extends GenericDao<Currency> {
     public CurrencyDao() {
         super(Currency.class);
+    }
+
+    @Transactional
+    public Currency getByCharCode(String charCode) {
+        Session currentSession = getSessionFactory().getCurrentSession();
+        CriteriaBuilder builder = currentSession.getCriteriaBuilder();
+        CriteriaQuery<Currency> cq = builder.createQuery(Currency.class);
+        Root<Currency> root = cq.from(Currency.class);
+        cq.where(builder.equal(root.get(Currency_.characterCode), charCode));
+        try {
+            return currentSession.createQuery(cq).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
     }
 }
