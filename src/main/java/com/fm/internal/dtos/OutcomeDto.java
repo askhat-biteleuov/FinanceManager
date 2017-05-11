6948209dd1cab0175430121e7a66a01b5de62e9d
KeiZaal <gafarov.dilshod@gmail.com>
2017-05-11 22:35:18 +0300

    add hashtag attribute in outcomedto

--- src/main/java/com/fm/internal/dtos/OutcomeDto.java
+++ src/main/java/com/fm/internal/dtos/OutcomeDto.java
@@ -12,6 +12,8 @@ public class OutcomeDto {
     @Pattern(regexp = "^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*(?:\\.[0-9]{2})?$")
     private String amount;
     private String defaultAmount;
+    @Pattern(regexp = "^((#(([^#\\s]+)\\s+)*(#([^#\\s]+)))|(#([^#\\s]+))?)$")
+    private String hashTags;
     @NotEmpty
     private String date;
     private String note;
@@ -87,5 +89,13 @@ public class OutcomeDto {
 
     public void setOutcomeType(OutcomeType outcomeType) {
         this.outcomeType = outcomeType;
+    }
+
+    public String getHashTags() {
+        return hashTags;
+    }
+
+    public void setHashTags(String hashTags) {
+        this.hashTags = hashTags;
     }
 }
